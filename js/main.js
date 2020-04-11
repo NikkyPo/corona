23c915145fcb6c23b0c8ef518bd4eb377abf86f0
helge312 <helge312@umn.edu>
2020-04-11 10:51:55 -0500

    added new COVID layer plus controls

--- js/main.js
+++ js/main.js
@@ -21,33 +21,200 @@ var counties = VectorTileLayer('https://www.sharedgeo.org/COVID-19/leaflet/data/
   }
 });
 
-var cases =  VectorTileLayer('https://www.sharedgeo.org/COVID-19/leaflet/data/county_cases/{z}/{x}/{y}.pbf', {
-  minDetailZoom: 0,
-  maxDetailZoom: 8,
-  style: function(f, name) {
-    const cases = f.properties.cases;
-    const ln_cases = Math.log(cases);
-    console.log(ln_cases);
-    const gb = 215 - Math.floor(ln_cases * 255 / 7);
-    console.log(gb);
-    const rgb = (255 << 16) + (gb << 8) + (gb)
-    console.log(rgb);
-    return ({
-      stroke: false,
-      fillColor: '#'+rgb.toString(16),
-      fill: true
-      });
+
+
+
+
+
+var displayDate = '2020-04-02';
+$.getJSON('https://www.sharedgeo.org/COVID-19/leaflet/data/covid-19-cases.json')
+ .done( data => {
+   let layer = VectorTileLayer('data/state_county/{z}/{x}/{y}.pbf', {
+     minDetailZoom: 0,
+     maxDetailZoom: 8,
+     attribution: "COVID-19 data is from the University of Virginia Biocomplexity Institute's COVID-19 Surveillance <a href='http://nssac.bii.virginia.edu/covid-19/dashboard/'>Dashboard</a> <a href='https://creativecommons.org/licenses/by-nc/4.0/'>CC-BY-NC</a>",
+     style: function(f, name) {
+       //console.log(f, name);
+       const state = f.properties.st_name;
+       const county = f.properties.cty_name;
+       const population = f.properties.tot_pop;
+
+       let r = 255;
+       let g = 255;
+       let b = 255;
+       if(data[state] &&
+        data[state]["counties"] &&
+        data[state]["counties"][county] &&
+        data[state]["counties"][county][displayDate]) {
+         const cases = 100000.0 * data[state]["counties"][county][displayDate] / population;
+         const log_cases = (cases > 1) ? Math.log10(cases) : 0;
+
+         if (log_cases <= 2.5) {
+           r = 255;
+           g = b = 255 - Math.floor(log_cases * 255 / 2.5);
+         } else {
+           b = Math.floor((log_cases - 2.5) * (255 / 2.5));
+           g = 0;
+           r = 255 - (b / 2);
+         }
+         //console.log(county, cases, log_cases, r, g, b);
+       }
+       const rgb = (255 << 16) + (g << 8) + (b);
+
+       return ({
+         stroke: false,
+         fillColor: '#'+rgb.toString(16),
+         fill: true,
+         fillOpacity: 0.6
+       });
+     },
+     zIndex: 1
+   });
+   //layer.addTo(mymap);
+   // layers.addBaseLayer(layer, "Active Cases per 100,000");
+
+  cases = VectorTileLayer('https://www.sharedgeo.org/COVID-19/leaflet/data/state_county/{z}/{x}/{y}.pbf', {
+     minDetailZoom: 0,
+     maxDetailZoom: 8,
+     attribution: "COVID-19 data is from the University of Virginia Biocomplexity Institute's COVID-19 Surveillance <a href='http://nssac.bii.virginia.edu/covid-19/dashboard/'>Dashboard</a> <a href='https://creativecommons.org/licenses/by-nc/4.0/'>CC-BY-NC</a>",
+     style: function(f, name) {
+       //console.log(f, name);
+       const state = f.properties.st_name;
+       const county = f.properties.cty_name;
+
+       let r = 255;
+       let g = 255;
+       let b = 255;
+       if(data[state] &&
+        data[state]["counties"] &&
+        data[state]["counties"][county] &&
+        data[state]["counties"][county][displayDate]) {
+         const cases = data[state]["counties"][county][displayDate];
+         const ln_cases = Math.log(cases);
+         if (ln_cases <= 7.0) {
+           r = 255;
+           g = b = 215 - Math.floor(ln_cases * 255 / 7);
+         } else {
+           b = Math.floor((ln_cases - 7) * (255 / 5));
+           g = 0;
+           r = 255 - (b / 2);
+         }
+       }
+       const rgb = (255 << 16) + (g << 8) + (b);
+
+       return ({
+         stroke: false,
+         fillColor: '#'+rgb.toString(16),
+         fill: true,
+         fillOpacity: 0.6
+       });
+     },
+    zIndex: 1
+   });
+   cases.addTo(mymap);
+   // layers.addBaseLayer(layer, "Active Cases");
+
+ });
+
+$("#date").change(function() {
+  displayDate = $("#date").val();
+  mymap.eachLayer( l => {
+    if(l.options.attribution && l.options.attribution.startsWith("COVID"))
+      l.redraw();
+  } );
+});
+
+
+let min = moment($("#date").attr("min"));
+let max = moment($("#date").attr("max"));
+let day = moment(min);
+
+function showNextDay() {
+  if (day < max) {
+    day.add(1, 'days');
+  } else {
+    day = moment(min);
   }
-});
-cases.bindPopup(function (layer) {
-  return L.Util.template('<p><strong>'+ layer.properties.st_name + '</strong><br></p><p>'+ layer.properties.cty_name +' county <br>'+ layer.properties.cases + ' cases<br> Updated: ' + layer.properties.updated_at +' </p>');
-});
+
+  $("#date").val(day.format('YYYY-MM-DD')).change();
+}
+
+function showPreviousDay() {
+  if (day > min) {
+    day.add(-1, 'days');
+  } else {
+    day = moment(max);
+  }
+
+  $("#date").val(day.format('YYYY-MM-DD')).change();
+}
+
+var timer = null;
+function startAnimation() {
+  if (timer == null) {
+    timer = setInterval( showNextDay, 2000 );
+  }
+}
+function stopAnimation() {
+  if (timer) {
+    clearInterval(timer);
+    timer = null;
+  }
+}
+
+
+
+
+
+
+
+
+// var cases =  VectorTileLayer('https://www.sharedgeo.org/COVID-19/leaflet/data/county_cases/{z}/{x}/{y}.pbf', {
+//   minDetailZoom: 0,
+//   maxDetailZoom: 8,
+//   style: function(f, name) {
+//     const cases = f.properties.cases;
+//     const ln_cases = Math.log(cases);
+//     console.log(ln_cases);
+//     const gb = 215 - Math.floor(ln_cases * 255 / 7);
+//     console.log(gb);
+//     const rgb = (255 << 16) + (gb << 8) + (gb)
+//     console.log(rgb);
+//     return ({
+//       stroke: false,
+//       fillColor: '#'+rgb.toString(16),
+//       fill: true
+//       });
+//   }
+// });
+// cases.bindPopup(function (layer) {
+//   return L.Util.template('<p><strong>'+ layer.properties.st_name + '</strong><br></p><p>'+ layer.properties.cty_name +' county <br>'+ layer.properties.cases + ' cases<br> Updated: ' + layer.properties.updated_at +' </p>');
+// });
 
 var boundaries = L.esri.featureLayer({ url: 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/Political_Boundaries_Area/FeatureServer/0'});
 boundaries.setStyle({
   color: 'grey',
   weight: 2,
   fill: false
+});
+
+/////////////////////////////////////////
+// Airports
+var airportsIcon = L.icon({
+	iconUrl: 'data/airport.svg',
+	iconSize: [25, 25],
+  popupAnchor: [0, -28]
+});
+var airports = L.esri.featureLayer({
+  url: "https://geo.dot.gov/server/rest/services/NTAD/Airports/MapServer/0",
+  pointToLayer: function (geojson, latlng) {
+  return L.marker(latlng, {
+      icon: airportsIcon
+    });
+  }
+ });
+airports.bindPopup(function (layer) {
+  return L.Util.template('<p><strong>{Fac_Name}</strong></p>', layer.feature.properties);
 });
 
 ////////////////////////////////////
@@ -259,34 +426,12 @@ va.bindPopup(function (layer) {
 });
 
 
-
-
-
-/////////////////////////////////////////
-// Airports
-var airportsIcon = L.icon({
-	iconUrl: 'data/airport.svg',
-	iconSize: [25, 25],
-  popupAnchor: [0, -28]
-});
-var airports = L.esri.featureLayer({
-  url: "https://geo.dot.gov/server/rest/services/NTAD/Airports/MapServer/0",
-  where: "STATE = 'MN'",
-  pointToLayer: function (geojson, latlng) {
-  return L.marker(latlng, {
-      icon: airportsIcon
-    });
-  }
- });
-airports.bindPopup(function (layer) {
-  return L.Util.template('<p><strong>{Fac_Name}</strong></p>', layer.feature.properties);
-});
-
 // Add it all together
 var mymap = L.map('mapid', {
+  preferCanvas: true,
   center: [45.9, -93.6],
   zoom: 6,
-  layers: [boundaries, none]
+  layers: [counties, boundaries, none]
   // layers: [counties, cases, boundaries, none]
 });
 
